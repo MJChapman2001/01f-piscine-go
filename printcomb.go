e99ff90bb52fc933f56a2158b0c79025153f6507
mjc2001 <mjchapman2001@gmail.com>
2021-12-01 09:55:51 +0000

    Unique combinations of three different digits

---
+++ printcomb.go
@@ -0,0 +1,27 @@
+package PrintComb
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintComb() {
+	firstDigit := '0'
+	secondDigit := '0'
+	thirdDigit := '0'
+
+	for firstDigit < '8' {
+		secondDigit = firstDigit + 1
+		thirdDigit = secondDigit + 1
+
+		for thirdDigit <= '9' {
+			z01.PrintRune(firstDigit)
+			z01.PrintRune(secondDigit)
+			z01.PrintRune(thirdDigit)
+			z01.PrintRune(',')
+			z01.PrintRune(' ')
+			z01.PrintRune('\n')
+			thirdDigit++
+		}
+		firstDigit++
+	}
+}
