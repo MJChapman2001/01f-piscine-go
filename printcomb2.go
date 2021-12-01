35965ad2091cb674fea20451a2504fda92933c11
mjc2001 <mjchapman2001@gmail.com>
2021-12-01 13:43:30 +0000

    All possible combinations of two different two-digit numbers

--- printcomb2.go
+++ printcomb2.go
@@ -7,25 +7,21 @@ import (
 func PrintComb2() {
 	for i := 0; i < 9900; i++ {
 		num4 := i % 10
-		num3 := (i % 100) - num4
-		num2 := (i % 1000) - (num4 + num3)
-		num1 := i - (num4 + num3 + num2)
+		num3 := ((i % 100) - num4) / 10
+		num2 := ((i % 1000) - (num4 + num3)) / 100
+		num1 := (i - (num4 + num3 + num2)) / 1000
 
-		if num1 == num3 && num2 == num4 {
-			continue
+		z01.PrintRune(rune(num1))
+		z01.PrintRune(rune(num2))
+		z01.PrintRune(' ')
+		z01.PrintRune(rune(num3))
+		z01.PrintRune(rune(num4))
+
+		if num1 == '9' && num2 == '8' && num3 == '9' && num4 == '9' {
+			z01.PrintRune('\n')
 		} else {
-			z01.PrintRune(rune(num1))
-			z01.PrintRune(rune(num2))
+			z01.PrintRune(',')
 			z01.PrintRune(' ')
-			z01.PrintRune(rune(num3))
-			z01.PrintRune(rune(num4))
-
-			if num1 == '9' && num2 == '8' && num3 == '9' && num4 == '9' {
-				z01.PrintRune('\n')
-			} else {
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
-			}
 		}
 	}
 }
