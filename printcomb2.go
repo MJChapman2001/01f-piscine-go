1ded58a3f3201d72586d5bdc639542c869278ceb
mjc2001 <mjchapman2001@gmail.com>
2021-12-01 13:25:20 +0000

    All possible combinations of two different two-digit numbers

--- printcomb2.go
+++ printcomb2.go
@@ -4,45 +4,28 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func Count(num rune) rune {
-	if num == '9' {
-		return '0'
-	} else {
-		return num + 1
-	}
-}
-
 func PrintComb2() {
-	num1 := '0'
-	num2 := '0'
-	num3 := '0'
-	num4 := '0'
+	for i := 0; i < 9900; i++ {
+		num4 := i % 10
+		num3 := (i % 100) - num4
+		num2 := (i % 1000) - (num4 + num3)
+		num1 := i - (num4 + num3 + num2)
 
-	for num1 <= '9' {
-		for num2 <= '9' {
-			for num3 <= '9' {
-				for num4 <= '9' {
-					if num1 == num3 && num2 == num4 {
-						continue
-					} else {
-						z01.PrintRune(num1)
-						z01.PrintRune(num2)
-						z01.PrintRune(' ')
-						z01.PrintRune(num3)
-						z01.PrintRune(num4)
-						if num1 == '9' && num2 == '8' && num3 == '9' && num4 == '9' {
-							z01.PrintRune('\n')
-						} else {
-							z01.PrintRune(',')
-							z01.PrintRune(' ')
-						}
-					}
-					Count(num4)
-				}
-				Count(num3)
+		if num1 == num3 && num2 == num4 {
+			continue
+		} else {
+			z01.PrintRune(rune(num1))
+			z01.PrintRune(rune(num2))
+			z01.PrintRune(' ')
+			z01.PrintRune(rune(num3))
+			z01.PrintRune(rune(num4))
+
+			if num1 == '9' && num2 == '8' && num3 == '9' && num4 == '9' {
+				z01.PrintRune('\n')
+			} else {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
 			}
-			Count(num2)
 		}
-		Count(num1)
 	}
 }
