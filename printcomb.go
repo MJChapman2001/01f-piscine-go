24777817bbda796d6269a72b2744204b00567995
mjc2001 <mjchapman2001@gmail.com>
2021-12-01 10:14:50 +0000

    Unique combinations of three different digits

--- printcomb.go
+++ printcomb.go
@@ -6,8 +6,8 @@ import (
 
 func PrintComb() {
 	for num1 := '0'; num1 < '8'; num1++ {
-		for num2 := '1'; num2 < '9'; num2++ {
-			for num3 := '2'; num3 <= '9'; num3++ {
+		for num2 := num1 + 1; num2 < '9'; num2++ {
+			for num3 := num2 + 1; num3 <= '9'; num3++ {
 				z01.PrintRune(num1)
 				z01.PrintRune(num2)
 				z01.PrintRune(num3)
