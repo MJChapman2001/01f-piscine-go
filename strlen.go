64c2c9b9665344ed70d46c1b1b52af7e4b9357ae
mjc2001 <mjchapman2001@gmail.com>
2021-12-02 10:11:54 +0000

    Length of a string

--- strlen.go
+++ strlen.go
@@ -1,5 +1,10 @@
 package StrLen
 
 func StrLen(s string) int {
-	return len(s)
+	runeArr := make([]rune, 0)
+
+	for i := 0; i < len(s); i++ {
+		runeArr = append(runeArr, rune(s[i]))
+	}
+	return len(runeArr)
 }
