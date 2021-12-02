198307fd63c5f3a26a3409d541e0101cedffc8d4
mjc2001 <mjchapman2001@gmail.com>
2021-12-02 10:41:19 +0000

    Reverse a string

--- strrev.go
+++ strrev.go
@@ -1,10 +1,9 @@
 package StrRev
 
 func StrRev(s string) string {
-	runeArr := []rune(s)
-	reverse := ""
-	for i := len(runeArr) - 1; i > 0; i++ {
-		reverse += string(runeArr[i])
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
 	}
-	return reverse
+	return string(runes)
 }
