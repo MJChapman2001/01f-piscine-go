faab9c162f5b4fb42cd7b6ac8393e5b87cdd02fe
mjc2001 <mjchapman2001@gmail.com>
2021-12-02 11:41:54 +0000

    Basic Atoi

--- basicatoi.go
+++ basicatoi.go
@@ -1,14 +1,14 @@
 package BasicAtoi
 
 func BasicAtoi(s string) int {
+	val := 0
 	for i := 0; i < len(s); i++ {
 		if i == len(s)-1 && string(s[i]) == "0" {
-			return 0
+			val += 0
 		} else if string(s[i]) == "0" {
 			continue
 		} else {
 			numArr := []rune(s[i:])
-			val := 0
 			for j, z := 0, len(numArr); j < len(numArr); j, z = j+1, z-1 {
 				pow := 10
 				for p := 0; p <= z; p++ {
@@ -20,7 +20,7 @@ func BasicAtoi(s string) int {
 				}
 				val += int(numArr[j]) * pow
 			}
-			return val
 		}
 	}
+	return val
 }
