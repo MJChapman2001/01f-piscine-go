0a1ac8c2eacbd69602570ee6c6ffe03520260bc5
mjc2001 <mjchapman2001@gmail.com>
2021-12-03 10:14:46 +0000

    Basic Atoi function

--- basicatoi.go
+++ basicatoi.go
@@ -2,18 +2,13 @@ package BasicAtoi
 
 func BasicAtoi(s string) int {
 	runeArr := []rune(s)
-	var val int64
-	val = 0
+	val := 0
 	for i, j := 0, len(runeArr); i < len(runeArr); i, j = i+1, j-1 {
-		pow := 10
-		if int(runeArr[i])-48 == 0 {
-			val += 0
-		} else {
-			for z := 1; z < j; z++ {
-				pow *= 10
-			}
-			val += (int(runeArr[i]) - 48) * pow
+		pow := 1
+		for z := 1; z < j; z++ {
+			pow *= 10
 		}
+		val += (int(runeArr[i]) - 48) * pow
 	}
 	return val
 }
