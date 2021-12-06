4f32e7f0d9e7aea91ee284d15df59616bd21660e
mjc2001 <mjchapman2001@gmail.com>
2021-12-06 15:21:48 +0000

    Trim Atoi

--- trimatoi.go
+++ trimatoi.go
@@ -3,13 +3,21 @@ package piscine
 func TrimAtoi(s string) int {
 	result := 0
 	sArr := []rune(s)
+	negative := false
 
 	for i := 0; i < len(sArr); i++ {
+		if s[i] == 45 && result == 0 {
+			negative = true
+		}
 		if IsNumeric(string(s[i])) {
 			result *= 10
 			result += int(sArr[i]) - 48
 		}
 	}
 
-	return result
+	if negative {
+		return -result
+	} else {
+		return result
+	}
 }
