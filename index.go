5cd7c30ee5de64832d53ffbe028eadb4fb8a663d
mjc2001 <mjchapman2001@gmail.com>
2021-12-06 13:20:03 +0000

    Index

--- index.go
+++ index.go
@@ -3,6 +3,9 @@ package piscine
 func Index(s string, toFind string) int {
 	for i := 0; i < len(s); i++ {
 		found := false
+		if len(toFind) == 0 {
+			break
+		}
 		if s[i] == toFind[0] {
 			for j := 0; j < len(toFind); j++ {
 				if s[i+j] == toFind[j] {
