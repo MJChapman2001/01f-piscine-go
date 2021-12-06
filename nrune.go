10d63626174337f0e4859aab4838005611903c0a
mjc2001 <mjchapman2001@gmail.com>
2021-12-06 11:54:40 +0000

    Nth rune of string

--- nrune.go
+++ nrune.go
@@ -3,7 +3,7 @@ package piscine
 func NRune(s string, n int) rune {
 	runeArr := []rune(s)
 
-	if n <= 0 || n >= len(runeArr) {
+	if n <= 0 || n > len(runeArr) {
 		return 0
 	} else {
 		return runeArr[n-1]
