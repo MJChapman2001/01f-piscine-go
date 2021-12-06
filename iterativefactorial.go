0957a26a641f1ee48c06302ebd1253f516a53cd2
mjc2001 <mjchapman2001@gmail.com>
2021-12-06 09:58:46 +0000

    Iterative factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,13 +3,11 @@ package piscine
 func IterativeFactorial(nb int) int {
 	result := 1
 
-	if nb <= 0 {
-		return 0
-	} else {
+	if nb > 0 {
 		for i := nb; i > 1; i-- {
 			result *= i
 		}
-
-		return result
 	}
+
+	return result
 }
