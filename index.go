f4d7da7d5b11d23401dce72441a5e1a879052ae0
mjc2001 <mjchapman2001@gmail.com>
2021-12-06 13:20:42 +0000

    Index

--- index.go
+++ index.go
@@ -4,7 +4,7 @@ func Index(s string, toFind string) int {
 	for i := 0; i < len(s); i++ {
 		found := false
 		if len(toFind) == 0 {
-			break
+			return 0
 		}
 		if s[i] == toFind[0] {
 			for j := 0; j < len(toFind); j++ {
