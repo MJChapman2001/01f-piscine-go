82c54b4dc1dc1179bd15580c37a01deb0f20e37a
mjc2001 <mjchapman2001@gmail.com>
2021-12-06 11:01:19 +0000

    Iterative power

---
+++ iterativepower.go
@@ -0,0 +1,14 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	if power < 0 {
+		return 0
+	}
+
+	result := nb
+	for i := 1; i < power; i++ {
+		result *= nb
+	}
+
+	return result
+}
