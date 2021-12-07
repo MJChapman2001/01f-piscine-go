ebe5906ee11e638d32d283feffedc069fbbe65e2
mjc2001 <mjchapman2001@gmail.com>
2021-12-07 12:18:32 +0000

    Print program name

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -10,8 +10,17 @@ func main() {
 	name := os.Args[0]
 
 	for i := 2; i < len(name); i++ {
-		z01.PrintRune(rune(name[i]))
+		if !IsPath(rune(name[i])) {
+			z01.PrintRune(rune(name[i]))
+		}
 	}
 
 	z01.PrintRune('\n')
+}
+
+func IsPath(r rune) bool {
+	if r == 46 || r == 47 {
+		return true
+	}
+	return false
 }
