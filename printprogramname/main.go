47d6ed2cc05264419a3f948fcf40a759d8a27c20
mjc2001 <mjchapman2001@gmail.com>
2021-12-07 10:39:02 +0000

    Print program name

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -9,9 +9,24 @@ import (
 func main() {
 	name := os.Args[0]
 
-	for i := 2; i < len(name); i++ {
-		z01.PrintRune(rune(name[i]))
+	for i := 0; i < len(name); i++ {
+		if IsAlpha(string(name[i])) {
+			z01.PrintRune(rune(name[i]))
+		}
 	}
 
 	z01.PrintRune('\n')
+}
+
+func IsAlpha(s string) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] < 48 || s[i] > 122 {
+			return false
+		} else if s[i] > 57 && s[i] < 65 {
+			return false
+		} else if s[i] > 90 && s[i] < 97 {
+			return false
+		}
+	}
+	return true
 }
