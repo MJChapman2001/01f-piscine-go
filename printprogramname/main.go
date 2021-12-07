4ebc432b1c40f4a685cf97c079c72c85ffbaefe3
mjc2001 <mjchapman2001@gmail.com>
2021-12-07 10:20:17 +0000

    Print program name

---
+++ printprogramname/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	name := os.Args[0]
+
+	for i := 0; i < len(name); i++ {
+		z01.PrintRune(rune(name[i]))
+	}
+
+	z01.PrintRune('\n')
+}
