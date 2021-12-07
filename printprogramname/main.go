3369f6945967333177fd549c4f815afae374125f
mjc2001 <mjchapman2001@gmail.com>
2021-12-07 10:34:29 +0000

    Print program name

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"os"
-	"piscine"
 
 	"github.com/01-edu/z01"
 )
@@ -11,9 +10,7 @@ func main() {
 	name := os.Args[0]
 
 	for i := 2; i < len(name); i++ {
-		if piscine.IsAlpha(string(name[i])) {
-			z01.PrintRune(rune(name[i]))
-		}
+		z01.PrintRune(rune(name[i]))
 	}
 
 	z01.PrintRune('\n')
