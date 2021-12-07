7c36a75b42082778ed71562a648cb26d5d77048c
mjc2001 <mjchapman2001@gmail.com>
2021-12-07 14:53:30 +0000

    Convert number to alpha

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -1,4 +1,62 @@
 package main
 
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
 func main() {
+	params := os.Args[1:]
+	var numArr []int
+	var runeArr []rune
+
+	if params[0] == "--upper" {
+		for i := 1; i < len(params); i++ {
+			numArr = append(numArr, BasicAtoi2(params[i]))
+		}
+
+		for j := 0; j < len(numArr); j++ {
+			if numArr[j] == 0 {
+				runeArr = append(runeArr, ' ')
+			} else {
+				runeArr = append(runeArr, ('A' + rune(numArr[j])))
+			}
+		}
+	} else {
+		for i := 0; i < len(params); i++ {
+			numArr = append(numArr, BasicAtoi2(params[i]))
+		}
+
+		for j := 0; j < len(numArr); j++ {
+			if numArr[j] == 0 {
+				runeArr = append(runeArr, ' ')
+			} else {
+				runeArr = append(runeArr, ('a' + rune(numArr[j])))
+			}
+		}
+	}
+
+	for k := 0; k < len(runeArr); k++ {
+		z01.PrintRune(runeArr[k])
+		z01.PrintRune('\n')
+	}
+}
+
+func BasicAtoi2(s string) int {
+	runeArr := []rune(s)
+	val := 0
+	for i, j := 0, len(runeArr); i < len(runeArr); i, j = i+1, j-1 {
+		pow := 1
+		if int(runeArr[i]) < 48 || int(runeArr[i]) > 57 {
+			val = 0
+			break
+		} else {
+			for z := 1; z < j; z++ {
+				pow *= 10
+			}
+			val += (int(runeArr[i]) - 48) * pow
+		}
+	}
+	return val
 }
