a06c6ae2407c717f33c3df0f6aff1bcb35a51f17
mjc2001 <mjchapman2001@gmail.com>
2021-12-08 10:46:40 +0000

    Concat params

--- concatparams.go
+++ concatparams.go
@@ -4,7 +4,11 @@ func ConcatParams(args []string) string {
 	result := ""
 
 	for i := 0; i < len(args); i++ {
-		result += args[i] + "\n"
+		if i < len(args)-1 {
+			result += args[i] + "\n"
+		} else {
+			result += args[i]
+		}
 	}
 
 	return result
