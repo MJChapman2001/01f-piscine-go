830245518886e85a11a16b434006f806a17e1574
mjc2001 <mjchapman2001@gmail.com>
2021-12-08 11:31:12 +0000

    Split

---
+++ split.go
@@ -0,0 +1,16 @@
+package piscine
+
+func Split(s, sep string) []string {
+	var output []string
+
+	for i, j := 0, 0; i < len(s); i++ {
+		if s[i:i+len(sep)] == sep {
+			output = append(output, s[j:i])
+			j = i + len(sep)
+		} else if i == len(s)-1 {
+			output = append(output, s[j:])
+		}
+	}
+
+	return output
+}
