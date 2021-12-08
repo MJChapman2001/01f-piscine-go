dfad23bb896a8c701e7a33c6f94ce7794b723fa1
mjc2001 <mjchapman2001@gmail.com>
2021-12-08 10:39:03 +0000

    Make range

---
+++ makerange.go
@@ -0,0 +1,16 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	if min >= max {
+		return []int(nil)
+	}
+
+	result := make([]int, max-min)
+
+	for i := 0; i < max-min; i++ {
+		result[i] = min
+		min++
+	}
+
+	return result
+}
