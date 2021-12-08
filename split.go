9ae0c0a7f13c6834f683b7eaf79b4d2c4531f102
mjc2001 <mjchapman2001@gmail.com>
2021-12-08 11:32:08 +0000

    Split

--- split.go
+++ split.go
@@ -4,7 +4,7 @@ func Split(s, sep string) []string {
 	var output []string
 
 	for i, j := 0, 0; i < len(s); i++ {
-		if s[i:i+len(sep)] == sep {
+		if s[i:i+len(sep)] == sep && i+len(sep) < len(s)-1 {
 			output = append(output, s[j:i])
 			j = i + len(sep)
 		} else if i == len(s)-1 {
