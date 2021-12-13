99377043c06ab2217fa7dde3ee1c8cb85734e07d
mjc2001 <mjchapman2001@gmail.com>
2021-12-13 12:04:36 +0000

    Is sorted

--- issorted.go
+++ issorted.go
@@ -1,9 +1,27 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) > 0 {
-			return false
+	val := 0
+	pos := 0
+
+	for val == 0 {
+		for pos < len(a)-1 {
+			val = f(a[pos], a[pos+1])
+			pos++
+		}
+	}
+
+	if val > 0 {
+		for i := pos; i < len(a)-1; i++ {
+			if f(a[i], a[i+1]) < 0 {
+				return false
+			}
+		}
+	} else if val < 0 {
+		for i := pos; i < len(a)-1; i++ {
+			if f(a[i], a[i+1]) > 0 {
+				return false
+			}
 		}
 	}
 
