53b542464d184c21ac5f248ee1e28b0d8a67d0ac
mjc2001 <mjchapman2001@gmail.com>
2021-12-13 13:56:27 +0000

    Split white spaces

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -5,7 +5,9 @@ func SplitWhiteSpaces(s string) []string {
 
 	for i, j := 0, 0; i < len(s); i++ {
 		if rune(s[i]) == ' ' || rune(s[i]) == '\n' {
-			output = append(output, s[j:i])
+			if i != j {
+				output = append(output, s[j:i])
+			}
 			j = i + 1
 		} else if i == len(s)-1 {
 			output = append(output, s[j:])
