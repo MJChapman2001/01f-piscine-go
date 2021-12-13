b86397796f8577f13516a4ff6661f4870876399f
mjc2001 <mjchapman2001@gmail.com>
2021-12-13 10:44:53 +0000

    cat

---
+++ cat/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) > 0 {
+		for i := range args {
+			file, err := ioutil.ReadFile(args[i])
+			var message string
+
+			if err != nil {
+				message = "ERROR: " + args[i] + ": No such file or directory"
+			} else {
+				message = string(file)
+			}
+
+			for j := range message {
+				z01.PrintRune(rune(message[j]))
+			}
+
+			z01.PrintRune('\n')
+		}
+	}
+}
