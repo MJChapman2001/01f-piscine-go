dd1c36efd8478185ca13b96ab66968ee28d1b50d
mjc2001 <mjchapman2001@gmail.com>
2021-12-13 09:48:06 +0000

    Point

--- point/main.go
+++ point/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"github.com/01-edu/z01"
+)
 
 type point struct {
 	x int
@@ -17,5 +19,17 @@ func main() {
 
 	setPoint(points)
 
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	output := ("x = !, y = *")
+
+	for i := range output {
+		if output[i] == '!' {
+			z01.PrintRune(rune(points.x))
+		} else if output[i] == '*' {
+			z01.PrintRune(rune(points.y))
+		} else {
+			z01.PrintRune(rune(output[i]))
+		}
+	}
+
+	z01.PrintRune('\n')
 }
