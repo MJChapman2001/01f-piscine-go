7a0170a2d5d51873728ef5ead5fa778cc79a66f1
mjc2001 <mjchapman2001@gmail.com>
2021-12-15 13:18:52 +0000

    Fix the main

--- fixthemain/main.go
+++ fixthemain/main.go
@@ -10,27 +10,29 @@ func PrintStr(s string) {
 	for _, r := range s {
 		z01.PrintRune(r)
 	}
+
+	z01.PrintRune('\n')
 }
 
 func OpenDoor(ptrDoor *Door) bool {
-	PrintStr("Door Opening...\n")
+	PrintStr("Door Opening...")
 	ptrDoor.state = "OPEN"
 	return true
 }
 
 func CloseDoor(ptrDoor *Door) bool {
-	PrintStr("Door Closing...\n")
+	PrintStr("Door Closing...")
 	ptrDoor.state = "CLOSE"
 	return true
 }
 
 func IsDoorOpen(ptrDoor *Door) bool {
-	PrintStr("is the Door opened ?\n")
+	PrintStr("is the Door opened ?")
 	return ptrDoor.state == "OPEN"
 }
 
 func IsDoorClose(ptrDoor *Door) bool {
-	PrintStr("is the Door closed ?\n")
+	PrintStr("is the Door closed ?")
 	return ptrDoor.state == "CLOSE"
 }
 
