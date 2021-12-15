eb2bc239964ecb33a4c284e54924540b6939c620
mjc2001 <mjchapman2001@gmail.com>
2021-12-15 14:11:39 +0000

    Compact

--- compact.go
+++ compact.go
@@ -4,17 +4,18 @@ func Compact(ptr *[]string) int {
 	temp := *ptr
 	var comp []string
 
-	for i := range temp {
-		if temp[i] == "" || temp[i] == " " {
-			comp = RemoveIndex(temp, i)
+	for i, j := 0, 0; i < len(temp); i++ {
+		if temp[i] == "" {
+			if i != j {
+				comp = append(comp, temp[j:i]...)
+			}
+			j = i + 1
+		} else if i == len(temp)-1 {
+			comp = append(comp, temp[j:]...)
 		}
 	}
 
 	*ptr = comp
 
 	return len(comp)
-}
-
-func RemoveIndex(s []string, i int) []string {
-	return append(s[:i], s[i+1:]...)
 }
