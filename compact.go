65b3aa4f038a73d0cb5200c5f5b6d577b0860ec4
mjc2001 <mjchapman2001@gmail.com>
2021-12-15 13:38:43 +0000

    Compact

--- compact.go
+++ compact.go
@@ -3,13 +3,22 @@ package piscine
 func Compact(ptr *[]string) int {
 	count := 0
 	temp := *ptr
+	comp := make([]string, len(temp))
 
 	for i := range temp {
-		if temp[i] == "" {
-			*ptr = temp[i+1:]
-			Compact(ptr)
-		} else {
+		if temp[i] != "" {
 			count++
+			for j := range comp {
+				if comp[j] == "" {
+					comp[j] = temp[i]
+				}
+			}
+		}
+	}
+
+	for k := range comp {
+		if comp[k] == "" {
+			*ptr = comp[:k]
 		}
 	}
 
