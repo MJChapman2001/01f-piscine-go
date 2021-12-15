57995a249a137aa649451f13578ee70be424e2db
mjc2001 <mjchapman2001@gmail.com>
2021-12-15 11:25:49 +0000

    Sort word arr

---
+++ sortwordarr.go
@@ -0,0 +1,12 @@
+package piscine
+
+func SortWordArr(a []string) {
+	for i := 0; i < len(a)-1; i++ {
+		if a[i] < a[i+1] {
+			temp := a[i]
+			a[i] = a[i+1]
+			a[i+1] = temp
+			SortWordArr(a)
+		}
+	}
+}
