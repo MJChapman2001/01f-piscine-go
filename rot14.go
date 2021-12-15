b1efb489f87a94bb0e96d6c6872d43e582fc8ae4
mjc2001 <mjchapman2001@gmail.com>
2021-12-15 12:05:08 +0000

    rot14

--- rot14.go
+++ rot14.go
@@ -7,13 +7,13 @@ func Rot14(s string) string {
 	for i := range arr {
 		if s[i] > 64 && s[i] < 91 {
 			if s[i]+14 > 90 {
-				arr[i] = rune((s[i] + 14) - (90 - 65))
+				arr[i] = rune((s[i] + 14) - (90 - 66))
 			} else {
 				arr[i] = rune(s[i] + 14)
 			}
 		} else if s[i] > 96 && s[i] < 123 {
 			if s[i]+14 > 122 {
-				arr[i] = rune((s[i] + 14) - (122 - 97))
+				arr[i] = rune((s[i] + 14) - (122 - 98))
 			} else {
 				arr[i] = rune(s[i] + 14)
 			}
