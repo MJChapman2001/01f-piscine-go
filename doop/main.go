9af787d468945060215fba3a7360ae21207741f6
mjc2001 <mjchapman2001@gmail.com>
2021-12-15 10:16:50 +0000

    Do op

--- doop/main.go
+++ doop/main.go
@@ -10,58 +10,52 @@ func main() {
 
 	if len(args) == 3 {
 		runnable := true
-		res := ""
 
 		for i := range args {
 			_, err := strconv.Atoi(args[i])
 			if err != nil {
 				runnable = false
-			} else {
-				res += args[i]
 			}
 		}
 
 		if runnable {
-			if args[1] == "/" && args[2] == "0" {
-				Zero(args[1])
-			} else if args[1] == "%" && args[2] == "0" {
-				Zero(args[1])
-			} else {
-				Run(args)
-			}
+			Maths(args)
 		}
 	}
 }
 
-func Run(s []string) int {
+func Maths(s []string) int {
 	val1, _ := strconv.Atoi(s[0])
 	val2, _ := strconv.Atoi(s[2])
 
-	if s[1] == "+" {
+	switch s[1] {
+	case "+":
 		return val1 + val2
-	} else if s[1] == "-" {
+	case "-":
 		return val1 - val2
-	} else if s[1] == "/" {
-		return val1 / val2
-	} else if s[1] == "*" {
+	case "*":
 		return val1 * val2
-	} else if s[1] == "%" {
-		return val1 % val2
+	case "/":
+		if val2 == 0 {
+			Zero(s[1])
+		} else {
+			return val1 / val2
+		}
+	case "%":
+		if val2 == 0 {
+			Zero(s[1])
+		} else {
+			return val1 % val2
+		}
 	}
 
 	return 0
 }
 
-func Zero(s string) string {
-	sign := ""
-
-	if s == "/" {
-		sign = "division"
+func Zero(sign string) string {
+	if sign == "/" {
+		return "No division by 0"
 	} else {
-		sign = "modulo"
+		return "No modulo by 0"
 	}
-
-	output := "No " + sign + " by 0"
-
-	return output
 }
