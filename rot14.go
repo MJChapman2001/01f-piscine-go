12a8fe4fc9411e86977a31c47f90afdaf1a74bff
mjc2001 <mjchapman2001@gmail.com>
2021-12-15 11:41:12 +0000

    rot14

---
+++ rot14.go
@@ -0,0 +1,26 @@
+package piscine
+
+func Rot14(s string) string {
+	arr := []rune(s)
+	output := ""
+
+	for i := range arr {
+		if s[i] > 64 && s[i] < 91 {
+			if s[i]+14 > 90 {
+				arr[i] = rune(65 + (s[i] - 90))
+			} else {
+				arr[i] = rune(s[i] + 14)
+			}
+		} else if s[i] > 96 && s[i] < 123 {
+			if s[i]+14 > 122 {
+				arr[i] = rune(97 + (s[i] - 122))
+			} else {
+				arr[i] = rune(s[i] + 14)
+			}
+		}
+
+		output += string(arr[i])
+	}
+
+	return output
+}
