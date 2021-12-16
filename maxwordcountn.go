31fb9f68be2fa188f077a86d6a6a69bbd35e23c9
mjc2001 <mjchapman2001@gmail.com>
2021-12-16 11:32:22 +0000

    Max word count N

---
+++ maxwordcountn.go
@@ -0,0 +1,65 @@
+package piscine
+
+import "sort"
+
+type Pair struct {
+	Key   string
+	Value int
+}
+
+type Pairlist []Pair
+
+func (p Pairlist) Len() int           { return len(p) }
+func (p Pairlist) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p Pairlist) Less(i, j int) bool { return p[i].Value < p[j].Value }
+
+func MaxWordCountN(text string, n int) map[string]int {
+	var textSlice []string
+
+	for i, j := 0, 0; i < len(text); i++ {
+		if rune(text[i]) == ' ' || rune(text[i]) == '\n' {
+			if i != j {
+				textSlice = append(textSlice, text[j:i])
+			}
+			j = i + 1
+		} else if i == len(text)-1 {
+			textSlice = append(textSlice, text[j:])
+		}
+	}
+
+	tempMap := make(map[string]int)
+
+	for _, i := range textSlice {
+		found := false
+
+		for j, _ := range tempMap {
+			if i == j {
+				found = true
+			}
+		}
+
+		if found {
+			tempMap[i]++
+		} else {
+			tempMap[i] = 1
+		}
+	}
+
+	sorted := make(Pairlist, len(tempMap))
+	index := 0
+
+	for k, v := range tempMap {
+		sorted[index] = Pair{k, v}
+		index++
+	}
+
+	sort.Sort(sorted)
+
+	output := make(map[string]int, n)
+
+	for i := 0; i < n; i++ {
+		output[sorted[i].Key] = sorted[i].Value
+	}
+
+	return output
+}
