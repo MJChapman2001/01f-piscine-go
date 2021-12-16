540df38a9f290d8522a0e6d30f444c586d9b18ed
mjc2001 <mjchapman2001@gmail.com>
2021-12-16 11:50:17 +0000

    Max word count N

--- maxwordcountn.go
+++ maxwordcountn.go
@@ -11,7 +11,7 @@ type Pairlist []Pair
 
 func (p Pairlist) Len() int           { return len(p) }
 func (p Pairlist) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
-func (p Pairlist) Less(i, j int) bool { return p[i].Value > p[j].Value }
+func (p Pairlist) Less(i, j int) bool { return p[j].Value < p[i].Value }
 
 func MaxWordCountN(text string, n int) map[string]int {
 	var textSlice []string
