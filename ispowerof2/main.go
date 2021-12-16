23983162e73d4aacf4ab4b429e2379b0c612af65
mjc2001 <mjchapman2001@gmail.com>
2021-12-16 10:15:25 +0000

    Is power of 2

---
+++ ispowerof2/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) == 1 {
+		input, err := strconv.Atoi(args[0])
+		var output string
+
+		if err == nil {
+			power := 2
+
+			for power < input {
+				power *= 2
+			}
+
+			if power == input {
+				output = "true"
+			} else {
+				output = "false"
+			}
+		}
+
+		PrintString(output)
+	}
+}
+
+func PrintString(s string) {
+	for i := range s {
+		z01.PrintRune(rune(s[i]))
+	}
+
+	z01.PrintRune('\n')
+}
