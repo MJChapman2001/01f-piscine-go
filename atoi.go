93c4f05dc946d8fe70574d88d558a9627900148e
mjc2001 <mjchapman2001@gmail.com>
2021-12-16 10:29:41 +0000

    Atoi

---
+++ atoi.go
@@ -0,0 +1,19 @@
+package piscine
+
+func Atoi(s string) int {
+	output := 0
+	sign := 1
+
+	for i := range s {
+		if s[i] == '-' && output == 0 {
+			sign = -1
+		}
+
+		if s[i] > 47 && s[i] < 58 {
+			output *= 10
+			output += int(s[i] - 48)
+		}
+	}
+
+	return output * sign
+}
